9d6ae9e43bdc3f6085854e719976ad2e7a442964
João Paulo <jpbsampaio@hotmail.com>
2024-12-31 14:19:45 -0300

    resolve problema no script

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var confirmation_button = document.getElementById("confirmation_button");
+  var confirmation_button = document.getElementById("clearCookies");
   confirmation_button.addEventListener('click', function() {
       chrome.extension.getBackgroundPage().clean_active_tab_cookie();
   });
